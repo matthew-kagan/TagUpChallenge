a74eb119aabfa0b4ba9e9d711380dcded528a376
Matthew Kagan <matthewkagan@Matthews-MBP-2.fios-router.home>
2020-05-06 20:48:37 -0400

    add comments

--- app.py
+++ app.py
@@ -11,12 +11,12 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
-
+#Default endpoint for testing
 @app.route("/")
 def test():
     return "Matt's TagUp Challenge"
 
-
+#Create endpoint to create new record
 @app.route("/api/create", methods=['POST'])
 def add_book():
     print(request.json.get("value1"))
@@ -33,7 +33,7 @@ def add_book():
     except Exception as e:
         return str(e)
 
-
+#List endpoint that returns all records
 @app.route("/api/list")
 def get_list():
     try:
@@ -42,7 +42,7 @@ def get_list():
     except Exception as e:
         return str(e)
 
-
+#Read endpoint that returns specific record by id
 @app.route("/api/read/<id_>")
 def get_by_id(id_):
     try:
@@ -51,7 +51,7 @@ def get_by_id(id_):
     except Exception as e:
         return str(e)
 
-
+#Delete endpoint that deletes specific record by id
 @app.route("/api/delete/<id_>")
 def delete_by_id(id_):
     try:
@@ -61,7 +61,7 @@ def delete_by_id(id_):
     except Exception as e:
         return str(e)
 
-
+#Modify endpoint that modifies specific record by id
 @app.route("/api/modify/<id_>", methods=['POST'])
 def modify_by_id(id_):
     try:
